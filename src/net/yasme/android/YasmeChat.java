5cee038a6174094f1f5bceaeef10261eb232e8d6
Robert Meissner <robert.meissner@studium.fau.de>
2014-05-29 11:10:35 +0200

    Refactored YasmeChat. Logic is now in entities.Chat

--- src/net/yasme/android/YasmeChat.java
+++ src/net/yasme/android/YasmeChat.java
@@ -1,20 +1,12 @@
 package net.yasme.android;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
-
-import javax.crypto.SecretKey;
-
-import net.yasme.android.connection.MessageTask;
+import net.yasme.android.entities.Chat;
 import net.yasme.android.entities.Message;
-import net.yasme.android.entities.User;
-import net.yasme.android.encryption.AESEncryption;
-import net.yasme.android.exception.RestServiceException;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -26,22 +18,14 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+
 public class YasmeChat extends Activity {
 	public final static String USER_NAME = "net.yasme.andriod.USER_NAME";
 	public final static String USER_ID = "net.yasme.andriod.USER_ID";
 
 	private EditText EditMessage;
 	private TextView status;
-
-	private String user_name;
-	private String user_id;
-	private String url;
-
-	int index = 0;
-
-	private MessageTask messageTask;
-
-	private AESEncryption aes;
+	private Chat chat;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,19 +37,16 @@ public class YasmeChat extends Activity {
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 		Intent intent = getIntent();
-		user_name = intent.getStringExtra(USER_NAME);
-		user_id = intent.getStringExtra(USER_ID);
-
-		aes = new AESEncryption("geheim");
-		url = getResources().getString(R.string.server_url);
-
+		String user_name = intent.getStringExtra(USER_NAME);
+		String user_id = intent.getStringExtra(USER_ID);
+		String url = getResources().getString(R.string.server_url);
+		chat = new Chat(user_name, user_id, url, this);
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 		initializeViews();
-		messageTask = new MessageTask(url);
 	}
 
 	@Override
@@ -73,75 +54,29 @@ public class YasmeChat extends Activity {
 		super.onStop();
 	}
 
+	public TextView getStatus() {
+		return status;
+	}
+
 	private void initializeViews() {
-		// chatView = new TextView[10];
 		EditMessage = (EditText) findViewById(R.id.text_message);
 		status = (TextView) findViewById(R.id.text_status);
-		status.setText("Eingeloggt: " + user_name);
+		status.setText("Eingeloggt: " + chat.getUser_name());
 	}
 
 	public void send(View view) {
-
 		String msg = EditMessage.getText().toString();
 
 		if (msg.isEmpty()) {
 			status.setText("Nichts eingegeben");
 			return;
 		}
-
+		chat.send(msg);
 		EditMessage.setText("");
 
-		new SendMessageTask().execute(msg, user_name);
-		update(view);
 		status.setText("Gesendet: " + msg);
 		msg = null;
 
-	}
-
-	private class SendMessageTask extends AsyncTask<String, Void, Boolean> {
-
-		String msg;
-
-		protected Boolean doInBackground(String... params) {
-
-			msg = params[0];
-			// encrypt message
-			String msg_encrypted = aes.encrypt(msg);
-
-			// creating message object
-			long uid = Long.parseLong(user_id);
-			boolean result = false;
-			try {
-				result = messageTask.sendMessage(new Message(uid, 2,
-						msg_encrypted, 0));
-			} catch (RestServiceException e) {
-				System.out.println(e.getMessage());
-			}
-			return result;
-		}
-
-		protected void onPostExecute(Boolean result) {
-			// if doInBackground returned true
-			// set Message to textViews
-			if (result) {
-				TextView textView = new TextView((getApplicationContext()));
-
-				LinearLayout layout = (LinearLayout) findViewById(R.id.scrollLayout);
-				LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-						LinearLayout.LayoutParams.WRAP_CONTENT,
-						LinearLayout.LayoutParams.WRAP_CONTENT);
-
-				LinearLayout row = new LinearLayout(getApplicationContext());
-				row.setLayoutParams(new LinearLayout.LayoutParams(
-						LinearLayout.LayoutParams.WRAP_CONTENT,
-						LinearLayout.LayoutParams.WRAP_CONTENT));
-
-				row.addView(textView);
-				layout.addView(row, layoutParams);
-			} else {
-				status.setText("Senden fehlgeschlagen");
-			}
-		}
 	}
 
 	public void update(View view) {
@@ -149,86 +84,42 @@ public class YasmeChat extends Activity {
 
 		// TODO: USERID nicht korrekt -> evtl falsch von Login abgespeichert;
 		// momentan USERID immer '001' - fixed
-		new GetMessageTask().execute(user_id);
+		chat.update();
 		status.setText("GET messages done");
 	}
 
-	private class GetMessageTask extends AsyncTask<String, Void, Boolean> {
-
-		ArrayList<Message> messages;
-
-		/**
-		 * @return Returns true if it was successful, otherwise false
-		 * @param params
-		 *            [0] is lastMessageID
-		 */
-		protected Boolean doInBackground(String... params) {
-
-			messages = messageTask.getMessage(params[0]);
-
-			if (messages.isEmpty()) {
-				return false;
-			}
-			if (messages.size() - 1 == index) {
-				return false;
-			}
-			int new_index = messages.size() - 1;
-
-			for (int i = 0; i <= index; i++) {
-				messages.remove(0);
-			}
-
-			// decrypt Messages
-			for (Message msg : messages) {
-				msg.setMessage(new String(aes.decrypt(msg.getMessage())));
-			}
-			index = new_index;
-			return true;
+	public void updateViews(ArrayList<Message> messages) {
+		if (messages.isEmpty()) {
+			status.setText("Keine neuen Nachrichten");
+			return;
 		}
+		Iterator<Message> iterator = messages.iterator();
+		Message msg = iterator.next();
+		for (int i = 0; i < messages.size(); i++) {
+			TextView textView = new TextView((getApplicationContext()));
+			TextView textView2 = new TextView((getApplicationContext()));
 
-		/**
-		 * Fills the TextViews with the messages
-		 * 
-		 * @param Gets
-		 *            the result of doInBackground
-		 */
-		protected void onPostExecute(Boolean result) {
-			if (result) {
-				if (messages.isEmpty()) {
-					status.setText("Keine neuen Nachrichten");
-					return;
-				}
-				Iterator<Message> iterator = messages.iterator();
-				Message msg = iterator.next();
-				for (int i = 0; i < messages.size(); i++) {
-					TextView textView = new TextView((getApplicationContext()));
-					TextView textView2 = new TextView((getApplicationContext()));
+			LinearLayout layout = (LinearLayout) findViewById(R.id.scrollLayout);
+			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+					LinearLayout.LayoutParams.WRAP_CONTENT,
+					LinearLayout.LayoutParams.WRAP_CONTENT);
 
-					LinearLayout layout = (LinearLayout) findViewById(R.id.scrollLayout);
-					LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-							LinearLayout.LayoutParams.WRAP_CONTENT,
-							LinearLayout.LayoutParams.WRAP_CONTENT);
+			LinearLayout row = new LinearLayout(getApplicationContext());
+			row.setLayoutParams(new LinearLayout.LayoutParams(
+					LinearLayout.LayoutParams.WRAP_CONTENT,
+					LinearLayout.LayoutParams.WRAP_CONTENT));
 
-					LinearLayout row = new LinearLayout(getApplicationContext());
-					row.setLayoutParams(new LinearLayout.LayoutParams(
-							LinearLayout.LayoutParams.WRAP_CONTENT,
-							LinearLayout.LayoutParams.WRAP_CONTENT));
+			textView.setText(msg.getSender() + ":");
+			textView2.setText(msg.getMessage());
+			row.addView(textView);
+			row.addView(textView2);
+			if (msg.getSender() == Long.parseLong(chat.getUser_id())) {
+				row.setGravity(Gravity.RIGHT);
+			}
+			layout.addView(row, layoutParams);
 
-					textView.setText(msg.getSender() + ":");
-					textView2.setText(msg.getMessage());
-					row.addView(textView);
-					row.addView(textView2);
-					if (msg.getSender() == Long.parseLong(user_id)) {
-						row.setGravity(Gravity.RIGHT);
-					}
-					layout.addView(row, layoutParams);
-
-					if (iterator.hasNext()) {
-						msg = iterator.next();
-					}
-				}
-			} else {
-				status.setText("Keine neuen Nachrichten");
+			if (iterator.hasNext()) {
+				msg = iterator.next();
 			}
 		}
 	}
--- src/net/yasme/android/entities/Chat.java
+++ src/net/yasme/android/entities/Chat.java
@@ -1,6 +1,11 @@
 package net.yasme.android.entities;
 
 import java.util.ArrayList;
+import net.yasme.android.YasmeChat;
+import net.yasme.android.connection.MessageTask;
+import net.yasme.android.encryption.AESEncryption;
+import net.yasme.android.exception.RestServiceException;
+import android.os.AsyncTask;
 
 
 /**
@@ -10,8 +15,28 @@ public class Chat {
 
 	private String chat_id;
 	private ArrayList<Message> messages;
-	private long lastMessageID;
-
+	//private long lastMessageID;
+	public long index;
+	
+	private String user_name;
+	private String user_id;
+	
+	private AESEncryption aes;
+	private MessageTask messageTask;
+	public YasmeChat activity;
+	
+	
+	/** Constructors **/
+	public Chat(String user_name, String user_id, String url, YasmeChat activity) {
+		this.user_name = user_name;
+		this.user_id = user_id;
+		aes = new AESEncryption("geheim");
+		messageTask = new MessageTask(url);
+		this.activity = activity;
+	}
+	
+	
+	/** Getters **/
 	public String getChat_id() {
 		return chat_id;
 	}
@@ -19,9 +44,103 @@ public class Chat {
 	public ArrayList<Message> getStoredMessages() {
 		return messages;
 	}
+	
+	public String getUser_name() {
+		return user_name;
+	}
+	
+	public String getUser_id() {
+		return user_id;
+	}
 
+	/** Setters **/
 	public void setMessages(ArrayList<Message> messages) {
 		this.messages = messages;
 	}
 	
+	
+	/** Other methods **/
+	public void send(String msg) {
+		new SendMessageTask().execute(msg, user_name);
+		update();
+	}
+	
+	public void update() {
+		new GetMessageTask().execute(user_id);
+	}
+	
+	private class SendMessageTask extends AsyncTask<String, Void, Boolean> {
+		String msg;
+
+		protected Boolean doInBackground(String... params) {
+
+			msg = params[0];
+			// encrypt message
+			String msg_encrypted = aes.encrypt(msg);
+
+			// creating message object
+			long uid = Long.parseLong(user_id);
+			boolean result = false;
+			try {
+				result = messageTask.sendMessage(new Message(uid, 2,
+						msg_encrypted, 0));
+			} catch (RestServiceException e) {
+				System.out.println(e.getMessage());
+			}
+			return result;
+		}
+
+		protected void onPostExecute(Boolean result) {
+			if(result) {
+				update();
+			} else {
+				activity.getStatus().setText("Senden fehlgeschlagen");
+			}
+		}
+	}
+	
+	
+	private class GetMessageTask extends AsyncTask<String, Void, Boolean> {
+		ArrayList<Message> messages;
+
+		/**
+		 * @return Returns true if it was successful, otherwise false
+		 * @param params [0] is lastMessageID
+		 */
+		protected Boolean doInBackground(String... params) {
+
+			messages = messageTask.getMessage(params[0]);
+
+			if (messages.isEmpty()) {
+				return false;
+			}
+			if (messages.size() - 1 == index) {
+				return false;
+			}
+			int new_index = messages.size() - 1;
+
+			for (int i = 0; i <= index; i++) {
+				messages.remove(0);
+			}
+
+			// decrypt Messages
+			for (Message msg : messages) {
+				msg.setMessage(new String(aes.decrypt(msg.getMessage())));
+			}
+			index = new_index;
+			return true;
+		}
+
+		/**
+		 * Fills the TextViews with the messages
+		 * @param Gets the result of doInBackground
+		 */
+		protected void onPostExecute(Boolean result) {
+			if(result) {
+				activity.updateViews(messages);
+			} else {
+				activity.getStatus().setText("Keine neuen Nachrichten");
+			}
+		}
+	}
 }
