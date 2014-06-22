fe9afd674e2bf26a50ba202159e5b2e83734486c
Benedikt Lorch <benedikt.lorch@studium.fau.de>
2014-06-22 14:31:45 +0200

    Implemented creation of new chat with selected participants

--- yasme/src/main/java/net/yasme/android/ui/InviteToChatFragment.java
+++ yasme/src/main/java/net/yasme/android/ui/InviteToChatFragment.java
@@ -2,7 +2,9 @@ package net.yasme.android.ui;
 
 import android.app.Fragment;
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import net.yasme.android.R;
+import net.yasme.android.asyncTasks.CreateChatTask;
 import net.yasme.android.asyncTasks.GetAllUsersTask;
 import net.yasme.android.entities.User;
 
@@ -31,6 +34,11 @@ public class InviteToChatFragment extends Fragment implements View.OnClickListen
     private ArrayAdapter<String> adapter;
 
     public InviteToChatFragment() { }
+
+
+    public AbstractYasmeActivity getAbstractYasmeActivity() {
+        return activity;
+    }
 
 
     @Override
@@ -63,11 +71,24 @@ public class InviteToChatFragment extends Fragment implements View.OnClickListen
             findViewsById();
         }
 
+        User self = activity.getSelfUser();
         this.users = users;
-        String[] userNames = new String[users.size()];
-        for (int i=0; i<users.size(); i++) {
-            userNames[i] = users.get(i).getName();
+
+        // Exclude self
+        String[] userNames = new String[users.size() - 1];
+
+        int i=0;
+        for (int cur=0; cur<users.size(); cur++) {
+            User user = users.get(cur);
+
+            // Skip self
+            if (user.getId() == self.getId()) {
+                continue;
+            }
+
+            userNames[i++] = user.getName();
         }
+
         adapter = new ArrayAdapter<>(activity, android.R.layout.simple_list_item_multiple_choice, userNames);
         chatPartners.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         chatPartners.setAdapter(adapter);
@@ -78,19 +99,35 @@ public class InviteToChatFragment extends Fragment implements View.OnClickListen
     @Override
     public void onClick(View v) {
         SparseBooleanArray checked = chatPartners.getCheckedItemPositions();
-        ArrayList<Long> selectedItems = new ArrayList<>();
-        ArrayList<String> selectedUsers = new ArrayList<>();
+        ArrayList<User> selectedUsers = new ArrayList<>();
+        ArrayList<String> selectedUserNames = new ArrayList<>();
+
+        if (checked.size() == 0) {
+            Toast.makeText(activity, getString(R.string.toast_no_selection), Toast.LENGTH_LONG).show();
+            return;
+        }
+
         for (int i = 0; i < checked.size(); i++) {
             // Item position in adapter
             int position = checked.keyAt(i);
-            // Add sport if it is checked i.e.) == TRUE!
             if (checked.valueAt(i)) {
-                selectedItems.add(users.get(position).getId());
-                selectedUsers.add(users.get(position).getName());
+                selectedUsers.add(users.get(position));
+                selectedUserNames.add(users.get(position).getName());
             }
         }
 
-        Toast.makeText(activity, "Selected: " + selectedUsers.toString(), Toast.LENGTH_LONG).show();
+        new CreateChatTask(activity.getApplicationContext(), this, selectedUsers).execute(Long.toString(activity.getUserId()), activity.getAccessToken());
         return;
+    }
+
+
+    public void startChat(long chatId) {
+        Log.d(this.getClass().getSimpleName(), "[DEBUG] Start chat: " + chatId);
+        Intent intent = new Intent(activity, ChatActivity.class);
+        intent.putExtra(activity.USER_MAIL, activity.getUserMail());
+        intent.putExtra(activity.USER_ID, activity.getUserId());
+        intent.putExtra(activity.CHAT_ID, chatId);
+        intent.putExtra(activity.USER_NAME, activity.getSelfUser().getName());
+        startActivity(intent);
     }
 }
