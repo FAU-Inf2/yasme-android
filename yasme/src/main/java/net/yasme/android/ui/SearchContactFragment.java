bd46b1874005b1a2c7cc1f291a64037525ec8386
Stefan Ettl <stefan.ettl@fau.de>
2014-06-22 14:48:19 +0200

    Added first design for Search User

--- yasme/src/main/java/net/yasme/android/ui/SearchContactFragment.java
+++ yasme/src/main/java/net/yasme/android/ui/SearchContactFragment.java
@@ -5,10 +5,23 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import net.yasme.android.R;
+import net.yasme.android.contacts.ContactListContent;
 
 public class SearchContactFragment extends Fragment implements ContactListItemFragment.OnFragmentInteractionListener {
+
+    private Spinner searchSpinner;
+    private Button searchButton;
+    private ListView searchResultView;
+    private TextView searchText;
+    private SimpleAdapter mAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,23 @@ public class SearchContactFragment extends Fragment implements ContactListItemFr
 
         View layout = inflater.inflate(R.layout.fragment_search_contact, null);
 
+        searchSpinner = (Spinner) layout.findViewById(R.id.search_spinner);
+        searchButton = (Button) layout.findViewById(R.id.search_button);
+        searchText = (TextView) layout.findViewById(R.id.search_text);
+        searchResultView = (ListView) layout.findViewById(R.id.search_listView);
+
+        this.loadSearchSpinner();
+
+        ContactListContent contactListContent = new ContactListContent();
+
+        ContactListContent.ContactListItem item = new ContactListContent.ContactListItem("1","test","test");
+        contactListContent.addItem(item);
+
+        mAdapter = new SimpleAdapter((ContactActivity)getActivity() ,
+                contactListContent.getMap(), android.R.layout.simple_list_item_2, new String[] {"name","mail"}, new int[]{android.R.id.text1,android.R.id.text2});
+
+        searchResultView.setAdapter(mAdapter);
+
         return layout;
     }
 
@@ -27,4 +57,14 @@ public class SearchContactFragment extends Fragment implements ContactListItemFr
     public void onFragmentInteraction(String id) {
 
     }
+
+    private void loadSearchSpinner(){
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.search_spinner_content, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        searchSpinner.setAdapter(adapter);
+    }
+
 }
