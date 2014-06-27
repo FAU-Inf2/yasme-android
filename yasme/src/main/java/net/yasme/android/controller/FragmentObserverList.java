f20c3ce9da4e7dec23ffbd643c9b7a10bbfc02bd
Andreas Kumlehn <andreas.kumlehn@cs.fau.de>
2014-06-27 23:26:18 +0200

    Fix for broken build

--- yasme/src/main/java/net/yasme/android/controller/FragmentObserverList.java
+++ yasme/src/main/java/net/yasme/android/controller/FragmentObserverList.java
@@ -8,20 +8,20 @@ import net.yasme.android.ui.ContactListItemFragment;
  */
 
 public class FragmentObserverList {
-    public static FragmentObserver1<ChatListFragment> chatListInstance;
-    public static FragmentObserver1<ContactListItemFragment> contactListInstance;
-
-    public static FragmentObserver1<ChatListFragment> getChatListInstance() {
-        if (chatListInstance == null) {
-            chatListInstance = new FragmentObserver1<ChatListFragment>();
-        }
-        return chatListInstance;
-    }
-
-    public static FragmentObserver1<ContactListItemFragment> getContactListInstance() {
-        if (contactListInstance == null) {
-            contactListInstance = new FragmentObserver1<ContactListItemFragment>();
-        }
-        return contactListInstance;
-    }
+//    public static FragmentObserver1<ChatListFragment> chatListInstance;
+//    public static FragmentObserver1<ContactListItemFragment> contactListInstance;
+//
+//    public static FragmentObserver1<ChatListFragment> getChatListInstance() {
+//        if (chatListInstance == null) {
+//            chatListInstance = new FragmentObserver1<ChatListFragment>();
+//        }
+//        return chatListInstance;
+//    }
+//
+//    public static FragmentObserver1<ContactListItemFragment> getContactListInstance() {
+//        if (contactListInstance == null) {
+//            contactListInstance = new FragmentObserver1<ContactListItemFragment>();
+//        }
+//        return contactListInstance;
+//    }
 }
