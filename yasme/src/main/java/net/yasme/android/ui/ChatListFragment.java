8b219275ceb6747b8942dc20c832712f6c3b12df
Martin Sturm <martin.sturm@informatik.stud.uni-erlangen.de>
2014-07-04 16:28:07 +0200

    Tried to expand observer to fragments - not working

--- yasme/src/main/java/net/yasme/android/ui/ChatListFragment.java
+++ yasme/src/main/java/net/yasme/android/ui/ChatListFragment.java
@@ -1,34 +1,27 @@
 package net.yasme.android.ui;
 
 import android.app.ListFragment;
-import android.app.Fragment;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 
 import net.yasme.android.R;
-import net.yasme.android.asyncTasks.GetChatDataTask;
-import net.yasme.android.asyncTasks.GetMessageTask;
+import net.yasme.android.asyncTasks.GetChatListTask;
 import net.yasme.android.asyncTasks.GetProfileDataTask;
 import net.yasme.android.asyncTasks.UpdateDBTask;
+import net.yasme.android.controller.NotifiableFragment;
+import net.yasme.android.controller.NotifyFragmentParameter;
+import net.yasme.android.controller.ObserverRegistry;
 import net.yasme.android.entities.Chat;
-import net.yasme.android.ui.*;
-import android.util.Log;
 
 import java.util.ArrayList;
 
 /**
  * Created by martin on 21.06.2014.
  */
-public class ChatListFragment extends ListFragment {
+public class ChatListFragment extends ListFragment implements NotifiableFragment<NotifyFragmentParameter> {
 
        private AbstractYasmeActivity activity;
        private ArrayList<Chat> chatRooms = new ArrayList<Chat>();
@@ -45,6 +38,9 @@ public class ChatListFragment extends ListFragment {
            adapter = new ChatListAdapter(activity, R.layout.chatlist_item, chatRooms);
            setListAdapter(adapter);
 
+            //Register at observer
+           //ChatListParam test = new ChatListParam(null);
+           ObserverRegistry.getRegistry(ObserverRegistry.Observers.CHATLISTFRAGMENT).register(this);
 
            new GetProfileDataTask(activity.getApplicationContext(), this, activity.storage)
                    .execute(Long.toString(activity.getUserId()), activity.getAccessToken(),
@@ -53,8 +49,15 @@ public class ChatListFragment extends ListFragment {
            new UpdateDBTask(activity.getApplicationContext(), activity.storage)
                    .execute(Long.toString(activity.getUserId()), activity.getAccessToken());
            //Laedt die Liste aller Chats von der Datenbank in das Fragment
-           new GetChatDataTask(this).execute();
+           new GetChatListTask(this).execute();
        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            //De-Register at observer
+            //ObserverRegistry.getRegistry(ObserverRegistry.Observers.CHATLISTFRAGMENT).remove(this);
+        }
         /*
        @Override
        public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -81,4 +84,49 @@ public class ChatListFragment extends ListFragment {
             intent.putExtra(activity.USER_NAME, activity.getSelfUser().getName());
             startActivity(intent);
         }
+
+       // @Override
+       // public void notifyFragment(ChatListParam value) {
+        //    //Log.d(this.getClass().getSimpleName(),"I have been notified. Yeeha!");
+        //}
+
+    @Override
+    public void notifyFragment(NotifyFragmentParameter value) {
+        Log.d(super.getClass().getSimpleName(),"I have been notified. Yeeha!");
+        ChatListAdapter adapter = (ChatListAdapter)this.getListAdapter();
+        //createDummyChatRoomList();
+        chatRooms = ((ChatListParam)value).getChatRooms();
+        adapter.updateChats(chatRooms);
+        adapter.notifyDataSetChanged();
+    }
+
+
+    public static class ChatListParam implements NotifyFragmentParameter {
+        public ArrayList<Chat> getChatRooms() {
+            return chatRooms;
+        }
+
+        private ArrayList<Chat> chatRooms = null;
+                public ChatListParam(ArrayList<Chat> chatRooms) {
+                    this.chatRooms = chatRooms;
+                }
+        }
+
+    //ChatListAdapter adapter = (ChatListAdapter)fragment.getListAdapter();
+    //Log.d(this.getClass().getSimpleName(), "UpdateMessages: " + chatRooms.size());
+    //adapter.updateChats(chatRooms);
+    //adapter.notifyDataSetChanged();
+
+
+    protected void createDummyChatRoomList() {
+        chatRooms = new ArrayList<Chat>();
+        int number = 10;
+        for (int i = 1; i < number; i++)
+        {
+            Chat chat = new Chat();
+            chat.setId(i);
+            chat.setName("Chat " + i);
+            chatRooms.add(chat);
+        }
+    }
 }
