67d8d765ee136cb4de85c55c96c0b3e30bb7aeef
Martin Sturm <martin.sturm@informatik.stud.uni-erlangen.de>
2014-07-22 15:11:43 +0200

    Enabled KeyEncryption

--- yasme/src/main/java/net/yasme/android/connection/MessageKeyTask.java
+++ yasme/src/main/java/net/yasme/android/connection/MessageKeyTask.java
@@ -72,9 +72,9 @@ public class MessageKeyTask extends ConnectionTask {
                 Log.d(this.getClass().getSimpleName(), "[???] MessageKey must be encrypted: "+ key);
 
                 //encrypt the key with RSA
-                //MessageSignature rsa = new MessageSignature();
-                //String keyEncrypted = rsa.encrypt(key, recipientDevice.getId());
-                String keyEncrypted = key;
+                MessageSignature rsa = new MessageSignature();
+                String keyEncrypted = rsa.encrypt(key, recipientDevice.getId());
+                //String keyEncrypted = key;
 
                 Log.d(this.getClass().getSimpleName(), "[???] MessageKey was encrypted: "+ keyEncrypted);
 
