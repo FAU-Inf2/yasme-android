16351a0b6e2511468301ca30150a93bd2e8ac601
Martin Sturm <martin.sturm@informatik.stud.uni-erlangen.de>
2014-08-03 20:14:55 +0200

    Added logging to Base64-class

--- yasme/src/main/java/net/yasme/android/encryption/Base64.java
+++ yasme/src/main/java/net/yasme/android/encryption/Base64.java
@@ -1,5 +1,7 @@
 package net.yasme.android.encryption;
 
+import android.util.Log;
+
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -8,14 +10,19 @@ import java.io.UnsupportedEncodingException;
 public class Base64 {
 
     public String base64Encode (byte[] input){
-        return android.util.Base64.encodeToString(input, android.util.Base64.DEFAULT);
+        String encoded = android.util.Base64.encodeToString(input, android.util.Base64.DEFAULT);
+        Log.d(getClass().getSimpleName(), "Base64-encode: " + encoded);
+        return encoded;
+
     }
 
     public byte[] base64Decode (String input, String coding) throws UnsupportedEncodingException {
+        Log.d(getClass().getSimpleName(), "Base64-decode: " + input);
         return android.util.Base64.decode(input.getBytes(coding), android.util.Base64.DEFAULT);
     }
 
     public byte[] base64Decode (String input){
+        Log.d(getClass().getSimpleName(), "Base64-decode: " + input);
         return android.util.Base64.decode(input.getBytes(), android.util.Base64.DEFAULT);
     }
 }
