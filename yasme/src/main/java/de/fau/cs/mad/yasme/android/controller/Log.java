be059520092737b998104c407dedd8db2e391a95
Martin Sturm <martin.sturm@informatik.stud.uni-erlangen.de>
2014-08-13 14:40:37 +0200

    Added own Log-class

---
+++ yasme/src/main/java/de/fau/cs/mad/yasme/android/controller/Log.java
@@ -0,0 +1,28 @@
+package de.fau.cs.mad.yasme.android.controller;
+
+/**
+ * Created by martin on 13.08.2014.
+ */
+public class Log {
+    static final boolean LOG_I = false;
+    static final boolean LOG_E = false;
+    static final boolean LOG_D = false;
+    static final boolean LOG_V = false;
+    static final boolean LOG_W = false;
+
+    public static void i(String tag, String string) {
+        if (LOG_I) android.util.Log.i(tag, string);
+    }
+    public static void e(String tag, String string) {
+        if (LOG_E) android.util.Log.e(tag, string);
+    }
+    public static void d(String tag, String string) {
+        if (LOG_D) android.util.Log.d(tag, string);
+    }
+    public static void v(String tag, String string) {
+        if (LOG_V) android.util.Log.v(tag, string);
+    }
+    public static void w(String tag, String string) {
+        if (LOG_W) android.util.Log.w(tag, string);
+    }
+}
