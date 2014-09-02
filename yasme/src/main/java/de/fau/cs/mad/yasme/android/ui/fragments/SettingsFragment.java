4f0c863e606e84034f023ad26f9faeb08b7f46b4
Robert Meissner <robert.meissner@studium.fau.de>
2014-09-02 17:33:00 +0200

    added strings and more switches

--- yasme/src/main/java/de/fau/cs/mad/yasme/android/ui/fragments/SettingsFragment.java
+++ yasme/src/main/java/de/fau/cs/mad/yasme/android/ui/fragments/SettingsFragment.java
@@ -17,7 +17,7 @@ import de.fau.cs.mad.yasme.android.ui.AbstractYasmeActivity;
  * Created by robert on 01.09.14.
  */
 public class SettingsFragment extends Fragment {
-    private Switch notification_sound, notification_vibration;
+    private Switch notificationSound, notificationVibration;
     private SharedPreferences.Editor settingsEditor;
 
     @Override
@@ -30,10 +30,10 @@ public class SettingsFragment extends Fragment {
         settingsEditor = DatabaseManager.INSTANCE.getSettings().edit();
 
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
-        notification_sound = (Switch) rootView.findViewById(R.id.switch1);
-        notification_vibration = (Switch) rootView.findViewById(R.id.switch2);
+        notificationSound = (Switch) rootView.findViewById(R.id.switch_notification_sound);
+        notificationVibration = (Switch) rootView.findViewById(R.id.switch_notification_vibration);
 
-        notification_sound.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        notificationSound.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // The toggle is enabled
@@ -46,7 +46,7 @@ public class SettingsFragment extends Fragment {
                 }
             }
         });
-        notification_vibration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        notificationVibration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // The toggle is enabled
