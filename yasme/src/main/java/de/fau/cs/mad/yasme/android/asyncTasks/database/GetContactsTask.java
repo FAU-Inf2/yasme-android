d51fa9a03ee9754b4557dd7eba064cc4b541a3e5
Robert Meissner <robert.meissner@studium.fau.de>
2014-09-10 19:04:29 +0200

    trying to avoid NullPointerException

--- yasme/src/main/java/de/fau/cs/mad/yasme/android/asyncTasks/database/GetContactsTask.java
+++ yasme/src/main/java/de/fau/cs/mad/yasme/android/asyncTasks/database/GetContactsTask.java
@@ -1,15 +1,14 @@
 package de.fau.cs.mad.yasme.android.asyncTasks.database;
 
 import android.os.AsyncTask;
-import de.fau.cs.mad.yasme.android.controller.Log;
 
 import java.util.List;
 
+import de.fau.cs.mad.yasme.android.controller.Log;
 import de.fau.cs.mad.yasme.android.controller.ObservableRegistry;
 import de.fau.cs.mad.yasme.android.controller.SpinnerObservable;
 import de.fau.cs.mad.yasme.android.entities.User;
 import de.fau.cs.mad.yasme.android.storage.DatabaseManager;
-import de.fau.cs.mad.yasme.android.storage.dao.UserDAO;
 import de.fau.cs.mad.yasme.android.ui.fragments.ChatSettingsAdd;
 import de.fau.cs.mad.yasme.android.ui.fragments.ContactListFragment;
 import de.fau.cs.mad.yasme.android.ui.fragments.InviteToChatFragment;
@@ -18,40 +17,39 @@ import de.fau.cs.mad.yasme.android.ui.fragments.InviteToChatFragment;
  * Created by Robert Meissner <robert.meissner@studium.fau.de> on 29.07.14.
  */
 public class GetContactsTask extends AsyncTask<String, Void, Boolean> {
-	private List<User> contacts;
-	private UserDAO userDao = DatabaseManager.INSTANCE.getUserDAO();
-	private Class classToNotify;
+    private List<User> contacts;
+    private Class classToNotify;
 
-	public GetContactsTask(Class classToNotify) {
-		this.classToNotify = classToNotify;
-	}
+    public GetContactsTask(Class classToNotify) {
+        this.classToNotify = classToNotify;
+    }
 
-	@Override
-	protected Boolean doInBackground(String... params) {
-		SpinnerObservable.getInstance().registerBackgroundTask(this);
-		contacts = userDao.getContacts();
-		if(contacts == null) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    protected Boolean doInBackground(String... params) {
+        SpinnerObservable.getInstance().registerBackgroundTask(this);
+        contacts = DatabaseManager.INSTANCE.getUserDAO().getContacts();
+        if (contacts == null) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	protected void onPostExecute(Boolean success) {
-		Log.d(this.getClass().getSimpleName(), "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-		SpinnerObservable.getInstance().removeBackgroundTask(this);
-		if (!success) {
-			Log.w(this.getClass().getSimpleName(), "Get contacts not successful");
-			return;
-		}
+    @Override
+    protected void onPostExecute(Boolean success) {
+        Log.d(this.getClass().getSimpleName(), "onPostExecute");
+        SpinnerObservable.getInstance().removeBackgroundTask(this);
+        if (!success) {
+            Log.w(this.getClass().getSimpleName(), "Get contacts not successful");
+            return;
+        }
 
-		// Notify
-		if(classToNotify != InviteToChatFragment.class && classToNotify != ContactListFragment.class && classToNotify != ChatSettingsAdd.class) {
-			Log.e(this.getClass().getSimpleName(), "classToNotify was not any listed known class.");
-			return;
-		}
+        // Notify
+        if (classToNotify != InviteToChatFragment.class && classToNotify != ContactListFragment.class && classToNotify != ChatSettingsAdd.class) {
+            Log.e(this.getClass().getSimpleName(), "classToNotify was not any listed known class.");
+            return;
+        }
 
-		ObservableRegistry.getObservable(classToNotify).
-			notifyFragments(new InviteToChatFragment.AllUsersFetchedParam(success, contacts));
-	}
+        ObservableRegistry.getObservable(classToNotify).
+                notifyFragments(new InviteToChatFragment.AllUsersFetchedParam(success, contacts));
+    }
 }
