97291825206416babbea1aae3531ee35ffa55c42
Robert Meissner <robert.meissner@studium.fau.de>
2014-09-26 12:12:13 +0200

    implemented the same code to reset the password in the settings

--- yasme/src/main/java/de/fau/cs/mad/yasme/android/ui/fragments/SettingsFragment.java
+++ yasme/src/main/java/de/fau/cs/mad/yasme/android/ui/fragments/SettingsFragment.java
@@ -5,19 +5,27 @@ import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import de.fau.cs.mad.yasme.android.R;
+import de.fau.cs.mad.yasme.android.asyncTasks.server.ChangePasswordTask;
+import de.fau.cs.mad.yasme.android.controller.Log;
+import de.fau.cs.mad.yasme.android.controller.Toaster;
+import de.fau.cs.mad.yasme.android.encryption.PasswordEncryption;
+import de.fau.cs.mad.yasme.android.entities.User;
 import de.fau.cs.mad.yasme.android.storage.DatabaseManager;
 import de.fau.cs.mad.yasme.android.ui.AbstractYasmeActivity;
 
@@ -85,7 +93,12 @@ public class SettingsFragment extends Fragment {
                 showLegal();
             }
         });
-
+        rootView.findViewById(R.id.button_reset_password).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showResetPassword();
+            }
+        });
 
         return rootView;
     }
@@ -153,6 +166,152 @@ public class SettingsFragment extends Fragment {
 
             }
         });
+        alert.show();
+    }
+
+    public void showResetPassword() {
+        AbstractYasmeActivity activity = (AbstractYasmeActivity) getActivity();
+        AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+        alert.setTitle(getString(R.string.request_email_title));
+
+        LinearLayout list = new LinearLayout(activity);
+        list.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        final TextView requestEmailText = new TextView(activity);
+        requestEmailText.setText(R.string.request_email_body);
+
+        final EditText mail = new EditText(activity);
+        mail.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        mail.setHint(R.string.registration_email);
+
+        list.addView(mail);
+        list.addView(requestEmailText, layoutParams);
+
+        alert.setView(list);
+
+        // "OK" button to save the values
+        alert.setPositiveButton(R.string.OK,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Grab the EditText's input
+                        String emailTmp = mail.getText().toString();
+                        Log.d(this.getClass().getSimpleName(), "Mail to send token at: " + emailTmp);
+                        User user = new User();
+                        user.setEmail(emailTmp);
+                        new ChangePasswordTask(user).execute("1");
+                        forgotPasswordDialog(emailTmp);
+                    }
+                }
+        );
+
+        // Skip, email was already sent
+        alert.setNeutralButton(R.string.skip,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        forgotPasswordDialog(null);
+                    }
+                }
+        );
+
+        // "Cancel" button
+        alert.setNegativeButton(R.string.cancel,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.cancel();
+                    }
+                }
+        );
+        alert.show();
+    }
+
+    public void forgotPasswordDialog(final String inputMail) {
+        AbstractYasmeActivity activity = (AbstractYasmeActivity) getActivity();
+        AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+        alert.setTitle(getString(R.string.password_title));
+
+        LinearLayout list = new LinearLayout(activity);
+        list.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        final EditText token = new EditText(activity);
+        token.setInputType(InputType.TYPE_CLASS_TEXT);
+        token.setHint(R.string.hint_mail_token);
+
+        final TextView tokenExplanation = new TextView(activity);
+        tokenExplanation.setText(R.string.explanation_mail_token);
+
+        final EditText password = new EditText(activity);
+        password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        password.setHint(R.string.hint_new_password);
+
+        final EditText passwordCheck = new EditText(activity);
+        passwordCheck.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        passwordCheck.setHint(R.string.hint_repeat_new_password);
+
+        final EditText mail = new EditText(activity);
+        mail.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        mail.setHint(R.string.registration_email);
+
+        if (inputMail == null || inputMail.isEmpty()) {
+            mail.setText(inputMail);
+            list.addView(mail);
+        }
+        list.addView(token, layoutParams);
+        list.addView(tokenExplanation, layoutParams);
+        list.addView(password, layoutParams);
+        list.addView(passwordCheck, layoutParams);
+
+        alert.setView(list);
+
+        // "OK" button to save the values
+        alert.setPositiveButton(R.string.OK,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                        // Grab the EditText's input
+                        String email;
+                        if (inputMail == null || inputMail.isEmpty()) {
+                            email = mail.getText().toString();
+                        } else {
+                            email = inputMail;
+                        }
+                        String mailToken = token.getText().toString();
+                        String inputPassword = password.getText().toString();
+                        String inputPasswordCheck = passwordCheck.getText()
+                                .toString();
+
+                        if (password.getText().length() < 8) {
+                            Toaster.getInstance().toast(R.string.password_too_short,
+                                    Toast.LENGTH_LONG);
+                            return;
+                        }
+                        if (!inputPassword.equals(inputPasswordCheck)) {
+                            Toaster.getInstance().toast(R.string.passwords_do_not_match,
+                                    Toast.LENGTH_LONG);
+                            return;
+                        }
+                        User user = new User(email, inputPassword);
+                        PasswordEncryption pwEnc = new PasswordEncryption(user);
+                        User secured = pwEnc.securePassword();
+                        new ChangePasswordTask(secured).execute("0", mailToken);
+                    }
+                }
+        );
+
+        // "Cancel" button
+        alert.setNegativeButton(R.string.cancel,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.cancel();
+                    }
+                }
+        );
         alert.show();
     }
 
